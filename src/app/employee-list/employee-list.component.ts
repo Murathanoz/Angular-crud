39b7c5f7884c37c563871825f611286b393ca00c
murat.oz <murat.oz@intecon.com.tr>
2022-09-28 15:05:30 +0300

    init commit

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees!: Employee[];
+  constructor(private employeeService: EmployeeService,private route:Router) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  private getEmployees() {
+    this.employeeService.getEmployeeList().subscribe(data=>
+      {
+        this.employees=data;
+      });
+  }
+
+  updateEmployee(id:number){
+    this.route.navigate(['update-employee',id]);
+  }
+
+  employeeDetails(id:number){
+    this.route.navigate(['employee-details',id]);
+  }
+
+  deleteEmployee(id:number){
+    this.employeeService.deleteEmployee(id).subscribe(data=>{
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+
+
+  
+
+
+}
